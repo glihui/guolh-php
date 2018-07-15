d75e76db54f3ff50ece422b1d62860e597ad8099
guolihui <15919476778@163.com>
2018-07-15 07:38:33 +0000

    add login back data

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\User;
+use App\Transformers\UserTransformer;
+use Dingo\Api\Http\Middleware\Auth;
 use Illuminate\Http\Request;
 use App\Http\Requests\Api\AuthorizationRequest;
 
 class AuthorizationsController extends Controller
 {
-    public function store(AuthorizationRequest $request)
+    public function store(AuthorizationRequest $request, User $user)
     {
         $credentials['name'] = $request->name;
         $credentials['password'] = $request->password;
@@ -17,11 +19,25 @@ class AuthorizationsController extends Controller
             return $this->response->errorUnauthorized('用户名或密码错误');
         }
 
-        return $this->response->array([
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-            'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
-        ])->setStatusCode(201);
+
+         $query = $user->query();
+         $query = $query->where('name', $request->name)->first();
+//         return $this->response->array([
+//             'ss' => $query
+//         ]);
+
+        return $this->response->item($query, new UserTransformer())
+            ->setMeta([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+            ])->setStatusCode(201);
+
+//        return $this->response->array([
+//            'access_token' => $token,
+//            'token_type' => 'Bearer',
+//            'expires_in' => \Auth::guard('api')->factory()->getTTL() * 60
+//        ])->setStatusCode(201);
     }
 
     protected function respondWithToken($token)
