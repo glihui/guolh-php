0f700ac52d3d2357321ae2aba60a58a441d39272
guolihui <15919476778@163.com>
2018-07-15 11:44:40 +0000

    add login back data

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -10,7 +10,7 @@ class UserObserver
     {
         // 这样写扩展性更高， 只有空的时候才指定默认头像
         if (empty($user->avatar)) {
-            $user->avatar = 'http://larabbs.test/uploads/images/avatars/201805/06/1_1525621949_ztDJfbjoYI.jpg';
+            $user->avatar = 'http://bbs.guolh.com/uploads/images/avatars/201801/17//12_1516122240_kr958NB7PE.jpg';
         }
     }
 }
