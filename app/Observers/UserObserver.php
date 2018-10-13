ce5bab3859364f40da894da4765755cf99e84fb3
郭黎辉 <guolihui@guolihuideMacBook-Pro.local>
2018-10-13 23:46:40 +0800

    add

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -10,7 +10,7 @@ class UserObserver
     {
         // 这样写扩展性更高， 只有空的时候才指定默认头像
         if (empty($user->avatar)) {
-            $user->avatar = 'http://bbs.guolh.com/uploads/images/avatars/201801/17//12_1516122240_kr958NB7PE.jpg';
+            $user->avatar = 'http://bbs.guolh.com/uploads/images/avatars/201810/13//1_1539417887_r1N0tbukAX.jpeg';
         }
     }
 }
